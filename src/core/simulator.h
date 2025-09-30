3fc641e6b98cd032ec7ebc6298c72927c4400ebb
Coldot <41678750+Coldot@users.noreply.github.com>
2025-09-30 11:58:33 +0900

    refactor: Simulator 클래스의 메서드 정리 및 사용자 입력 시뮬레이션 기능 추가

--- src/core/simulator.h
+++ src/core/simulator.h
@@ -12,46 +12,15 @@
 
 using namespace std;
 
-enum SimulationCommand {
-    ADD_ORDERER,
-    ADD_STORE,
-    ADD_DRIVER,
-    PLACE_ORDER,
-    VIEW_STATUS,
-    VIEW_DRIVER_STATUS,
-    VIEW_STORE_STATUS,
-    RUN_SIMULATION_STEP,
-    EXIT_SIMULATION
-};
-
 class Simulator {
-    public:
-        Simulator();
-        ~Simulator();
+public:
+    Simulator();
+    ~Simulator();
+    
+    void simulateWithUserInput();
 
-        // Initialization methods
-        void initializeSystem();
-        void setupDefaultParticipants();
-        void addOrderer(int id, const string& name, int x, int y);
-        void addStore(int id, const string& name, int x, int y);
-        void addDriver(int id, const string& name, int x, int y);
-
-        // Simulation control methods
-        void startSimulation();
-        void runSimulationStep();
-        void processUserCommand();
-        void executeCommand(SimulationCommand command, const vector<string>& params);
-
-    private:
-        DeliverySystem* deliverySystem;
-        Map* map;
-        bool simulationRunning;
-        int simulationStep;
-
-        // Default setup methods
-        void createDefaultOrderers();
-        void createDefaultStores();
-        void createDefaultDrivers();
+private:
+    DeliverySystem* deliverySystem;
 };
 
 #endif
