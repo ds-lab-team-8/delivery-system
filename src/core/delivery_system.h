cff00759a8b158552392e34638ed5d35bb9bb88a
Coldot <41678750+Coldot@users.noreply.github.com>
2025-09-30 11:58:27 +0900

    refactor: DeliverySystem 클래스의 메서드 정리 및 추가

--- src/core/delivery_system.h
+++ src/core/delivery_system.h
@@ -14,52 +14,24 @@
 using namespace std;
 
 class DeliverySystem {
-    public:
-        DeliverySystem();
-        ~DeliverySystem();
+public:
+    DeliverySystem();
+    ~DeliverySystem();
 
-        // Participant management
-        void addOrderer(const Orderer& orderer);
-        void addStore(const Store& store);
-        void addDriver(const Driver& driver);
+    void addOrderer(const Orderer& orderer);
+    void addStore(const Store& store);
+    void addDriver(const Driver& driver);
+    void addOrder(const Order& order);
 
-        // Order lifecycle methods
-        void processNewOrder(int ordererId, int storeId, const string& orderDetails);
-        void storeRequestDriverCall();
-        void driverViewAvailableOrders(int driverId);
-        void driverAcceptOrder(int driverId, int orderId);
-        void storeSetPickupComplete(int orderId);
-        void driverCompleteDelivery(int orderId);
+    void processOrdersWithDriverCall();
+    void processOrdersWithSystemSelection();
 
-        // Optimization algorithms (core features)
-        int findOptimalDriver(const Order& order);
-        vector<Order*> optimizeMultipleDeliveries(int driverId);
-        double calculateDeliveryDistance(const Location& store, const Location& customer, const Location& driverPos);
-
-        // Getters
-        vector<Orderer>& getOrderers();
-        vector<Store>& getStores();
-        vector<Driver>& getDrivers();
-        Order* findOrderById(int orderId);
-        Store* findStoreById(int storeId);
-        Driver* findDriverById(int driverId);
-        Orderer* findOrdererById(int ordererId);
-
-    private:
-        Map map;
-        vector<Orderer> orderers;
-        vector<Driver> drivers;
-        vector<Store> stores;
-        vector<Order*> orders;
-        queue<Order*> ordersWaitingStore;
-        queue<Order*> ordersWaitingDriver;
-        int nextOrderId;
-
-        // Internal helper methods
-        void autoProcessStoreOrders();
-        void autoAssignDrivers();
-        void updateOrderStatus(int orderId, OrderStatus newStatus);
-        double calculateDistance(const Location& a, const Location& b) const;
+private:
+    Map map;
+    vector<Orderer> orderers;
+    vector<Driver> drivers;
+    vector<Store> stores;
+    vector<Order*> orders;
 };
 
 #endif
