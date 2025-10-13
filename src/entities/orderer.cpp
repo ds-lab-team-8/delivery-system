0cdcf9e3411910f57bb9f638216d4de56b4e2af0
siyoung <pangpangsy20@gmail.com>
2025-10-14 04:28:47 +0900

    e

---
+++ src/entities/orderer.cpp
@@ -0,0 +1,25 @@
+#include "orderer.h"
+
+// Constructor
+Orderer::Orderer(int id_in, const string& name_in, const Location& location_in) 
+    : id(id_in), name(name_in), location(location_in) {
+    
+}
+
+// Destructor
+Orderer::~Orderer() {
+
+}
+
+//Getters
+int Orderer::getId() const{
+    return id;
+}
+
+string Orderer::getName() const {
+    return name;
+}
+
+Location Orderer::getLocation() const {
+    return location;
+}
---
+++ src/entities/store.cpp
@@ -0,0 +1,58 @@
+#include "store.h"
+#include "order.h"
+#include "../core/delivery_system.h"
+
+// Constructor
+Store::Store(int id_in, const string& name_in, const Location& location_in) 
+    :id(id_in), name(name_in), location(location_in) {
+
+}
+
+// Destructor
+Store::~Store() {
+
+}
+
+// Getters
+int Store::getId() const {
+    return id;
+}
+
+string Store::getName() const {
+    return name;
+}
+
+Location Store::getLocation() const {
+    return location;
+}
+
+queue<Order*> Store::getOrderQueue() const {
+    return orderQueue;
+}
+
+// Order management
+void Store::receiveOrder(Order* order) {
+    order -> acceptOrder(); // ORDER_ACCEPTED로 변경
+    orderQueue.push(order);
+}
+
+Order* Store::processNextOrder() {
+    if(!orderQueue.empty()) {
+        Order* orderToProcess = orderQueue.front();
+        orderQueue.pop();
+        return orderToProcess;
+    }
+    return nullptr; // 처리할 주문 없을 시
+}
+
+void Store::requestDriverCall(DeliverySystem* system) {
+    system -> requestCallsToDrivers();
+}
+
+void Store::setPickupComplete(int orderId) {
+
+}
+
+void Store::displayOrderQueue() const {
+    
+}
