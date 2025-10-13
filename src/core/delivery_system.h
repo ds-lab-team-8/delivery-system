79173ee9e681acc9c2df77278d543f26a95d7f6b
Coldot <41678750+Coldot@users.noreply.github.com>
2025-10-13 22:29:12 +0900

    chore: delivery_system.h에 TODO 주석 추가 및 자료구조 개선 계획 명시

--- src/core/delivery_system.h
+++ src/core/delivery_system.h
@@ -26,6 +26,7 @@ public:
 
     // 배차 및 주문 처리 단계의 메서드들
     void requestCallsToDrivers();   // 현재 orders 내에 있는 모든 주문들을 drivers에게 배차 요청 (driver의 배차 큐에 추가)
+    // TODO: 실패 시 예외 발생 처리
     void acceptCall(int orderId);   // 특정 주문을 배차 요청에 수락 (driver가 호출)
     void completePickup(int orderId);   // 특정 주문을 픽업 완료 (주문 상태 변경)
     void completeDelivery(int orderId);   // 특정 주문을 배달 완료 (주문 상태 변경)
@@ -35,6 +36,7 @@ private:
     vector<Orderer> orderers;
     vector<Driver> drivers;
     vector<Store> stores;
+    // TODO: Queue 자료구조로 수정
     vector<Order*> orders;
 };
 
