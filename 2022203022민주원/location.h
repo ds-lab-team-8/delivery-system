4d46ae7c9a51c47c4490aa6f07355e4008739e17
minjoowon <148769629+minjoowon@users.noreply.github.com>
2025-10-24 19:17:24 +0900

    Create location.h

---
+++ 2022203022민주원/location.h
@@ -0,0 +1,34 @@
+#ifndef LOCATION_H
+#define LOCATION_H
+
+#include <iostream>
+
+using namespace std;
+
+class Location {
+    public:
+        Location();
+        Location(int x, int y);
+        ~Location();
+
+        // Getters
+        int getX() const;
+        int getY() const;
+
+        // Setters
+        void updateLocation(int x, int y);
+
+        // Utility methods
+        double calculateDistance(const Location& other) const;
+        bool operator==(const Location& other) const;
+        bool operator!=(const Location& other) const;
+
+
+        int node;
+
+    private:
+        int x;
+        int y;
+};
+
+#endif
