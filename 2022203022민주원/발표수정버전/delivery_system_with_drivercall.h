3aeb6d6e80336fea77b8811c49f96a01c6bf4e19
minjoowon <148769629+minjoowon@users.noreply.github.com>
2025-12-14 11:48:25 +0900

    Create delivery_system_with_drivercall.h

---
+++ 2022203022민주원/발표수정버전/delivery_system_with_drivercall.h
@@ -0,0 +1,20 @@
+#ifndef DELIVERY_SYSTEM_WITH_DRIVER_CALL_H
+#define DELIVERY_SYSTEM_WITH_DRIVER_CALL_H
+
+#include "delivery_system.h"
+
+class DeliverySystemWithDriverCall : public DeliverySystem {
+public:
+	DeliverySystemWithDriverCall();
+	~DeliverySystemWithDriverCall();
+
+	void acceptCall() override;
+
+protected:
+	vector<vector<Order*>> generateOrderCombos(const vector<Order*>& availableOrders, int maxComboSize = 3);
+	double bestDistanceForOrderCombo(const vector<Order*>& orderCombo, const Driver& driver, const Map& map);
+	double computeEfficiency(const vector<Order*>& group, double totalDist);
+
+};
+
+#endif
