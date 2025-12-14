fd832f7553ab5555ed08179dadb7e11d2e05b9b1
minjoowon <148769629+minjoowon@users.noreply.github.com>
2025-12-14 11:55:43 +0900

    Create store.cpp

---
+++ 2022203022민주원/발표수정버전/store.cpp
@@ -0,0 +1,70 @@
+#include "store.h"
+#include "order.h"
+#include "delivery_system.h"
+
+// Constructor
+Store::Store(int id_in, const string& name_in, const Location& location_in, double feePerDistance_in)
+    :id(id_in), name(name_in), location(location_in), feePerDistance(feePerDistance_in) {
+
+}
+
+// Destructor
+Store::~Store() {
+
+}
+
+// Getters
+int Store::getId() const {
+    return id;
+}
+
+string Store::getName() const {
+    return name;
+}
+
+Location Store::getLocation() const {
+    return location;
+}
+
+queue<Order*> Store::getOrderQueue() const {
+    return orderQueue;
+}
+
+void Store::setLocationNode(int node) {
+    location.node = node;
+}
+
+double Store::getFeePerDistance() const {
+    return feePerDistance;
+}
+
+// Order management
+void Store::receiveOrder(Order* order) {
+    order->acceptOrder(); // ORDER_ACCEPTED로 변경
+    orderQueue.push(order);
+}
+
+Order* Store::processNextOrder() {
+    if (!orderQueue.empty()) {
+        Order* orderToProcess = orderQueue.front();
+        orderQueue.pop();
+        return orderToProcess;
+    }
+    return nullptr; // 처리할 주문 없을 시
+}
+
+void Store::requestDriverCall(DeliverySystem* system) {
+    system->acceptCall();
+}
+
+void Store::setPickupComplete(int orderId) {
+
+}
+
+void Store::displayOrderQueue() const {
+
+}
+
+bool Store::hasOrdersWaiting() const {
+    return !orderQueue.empty();
+}
