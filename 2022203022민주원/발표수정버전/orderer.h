96d1971c46f41b2aac02c03ab0122cedd11ddf66
minjoowon <148769629+minjoowon@users.noreply.github.com>
2025-12-14 11:54:27 +0900

    Create orderer.h

---
+++ 2022203022민주원/발표수정버전/orderer.h
@@ -0,0 +1,29 @@
+#ifndef ORDERER_H
+#define ORDERER_H
+
+#include <iostream>
+#include <string>
+#include "location.h"
+
+using namespace std;
+
+class Orderer {
+public:
+    Orderer(int id, const string& name, const Location& location);
+    ~Orderer();
+
+    // Getters
+    int getId() const;
+    string getName() const;
+    Location getLocation() const;
+
+    // Location node 설정
+    void setLocationNode(int node);
+
+private:
+    int id;
+    string name;
+    Location location;
+};
+
+#endif
