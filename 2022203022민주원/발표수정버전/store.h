d3e96870469390395c226f4d27527b3f465932b0
minjoowon <148769629+minjoowon@users.noreply.github.com>
2025-12-14 11:56:02 +0900

    Create store.h

---
+++ 2022203022민주원/발표수정버전/store.h
@@ -0,0 +1,45 @@
+#ifndef STORE_H
+#define STORE_H
+
+#include <iostream>
+#include <string>
+#include <queue>
+#include "location.h"
+
+using namespace std;
+
+class Order;
+class DeliverySystem;
+
+class Store {
+public:
+    Store(int id, const string& name, const Location& location, double feePerDistance);
+    ~Store();
+
+    // Getters
+    int getId() const;
+    string getName() const;
+    Location getLocation() const;
+    queue<Order*> getOrderQueue() const;
+
+    // Location node 설정
+    void setLocationNode(int node);
+
+    // Order management methods
+    void receiveOrder(Order* order);
+    Order* processNextOrder();
+    void requestDriverCall(DeliverySystem* system);
+    void setPickupComplete(int orderId);
+    bool hasOrdersWaiting() const;
+    void displayOrderQueue() const;
+    double getFeePerDistance() const;
+
+private:
+    int id;
+    string name;
+    Location location;
+    queue<Order*> orderQueue;
+    double feePerDistance;
+};
+
+#endif
