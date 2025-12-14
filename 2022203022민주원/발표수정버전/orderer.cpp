1a4bc04e5f81f84296a5774921cd0d857097b6fa
minjoowon <148769629+minjoowon@users.noreply.github.com>
2025-12-14 11:54:16 +0900

    Create orderer.cpp

---
+++ 2022203022민주원/발표수정버전/orderer.cpp
@@ -0,0 +1,29 @@
+#include "orderer.h"
+
+// Constructor
+Orderer::Orderer(int id_in, const string& name_in, const Location& location_in)
+    : id(id_in), name(name_in), location(location_in) {
+
+}
+
+// Destructor
+Orderer::~Orderer() {
+
+}
+
+//Getters
+int Orderer::getId() const {
+    return id;
+}
+
+string Orderer::getName() const {
+    return name;
+}
+
+Location Orderer::getLocation() const {
+    return location;
+}
+
+void Orderer::setLocationNode(int node) {
+    location.node = node;
+}
