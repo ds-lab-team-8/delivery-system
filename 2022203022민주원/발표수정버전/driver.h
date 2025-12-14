aea8c6578bdea4a25c3eb66c419857f30457ddf5
minjoowon <148769629+minjoowon@users.noreply.github.com>
2025-12-14 11:52:18 +0900

    Create driver.h

---
+++ 2022203022민주원/발표수정버전/driver.h
@@ -0,0 +1,52 @@
+#ifndef DRIVER_H
+#define DRIVER_H
+
+#include <iostream>
+#include <string>
+#include <queue>
+#include "location.h"
+
+using namespace std;
+
+class Order;
+class DeliverySystem;
+
+class Driver {
+public:
+    Driver(int id, const string& name, const Location& initialLocation,int n);
+    ~Driver();
+
+    // Getters
+    int getId() const;
+    string getName() const;
+    Location getCurrentLocation() const;
+    bool isAvailable() const;
+    double getTotalEarnings() const;
+
+    // Location management
+    void updateLocation(const Location& newLocation);
+    void setLocationNode(int node);
+
+    // Order management methods
+    void addOrder(Order* order);
+    void acceptOrder(int orderId, DeliverySystem* system);
+    void completeDelivery(int orderId);
+    void addEarnings(double amount);
+
+    // 조회용 (Simulator가 DS 큐 순서를 조회할 때 사용 가능)
+    int getPendingOrderCount() const;
+    vector<int> getQueuedOrderIds() const;
+
+    Location currentLocation;
+
+    int currentNode;
+private:
+    int id;
+    string name;
+    
+    queue<Order*> orderQueue;
+    bool available;
+    double totalEarnings;
+};
+
+#endif
